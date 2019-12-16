2b9fa88e9a27c666aedce63368294a2b5fafb7cb
Phondanai Khanti <phondanai@gmail.com>
2019-12-16 12:08:43 +0700

    Chapter 2: Templates | Flask MEGA tutorial

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {"username": "Phondanai"}
+    posts = [
+        {
+            "author": {"username": "John"},
+            "body": "Beautiful day in Poland!"
+        },
+        {
+            "author": {"username": "Susan"},
+            "body": "The Avengers movie was so cool!"
+        },
+
+    ]
+    return render_template('index.html', user=user, posts=posts)
